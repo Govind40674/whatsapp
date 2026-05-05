259680c75756cb8d2e55dc7ef5b8d3459df0e3c8
Govind40674 <govindkumar4067444@outlook.com>
2026-05-05 10:08:16 +0530

    changes

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FaEllipsisV } from "react-icons/fa";
-import {socket} from "../../chat/socket"
+import { socket } from "../chat/socket"
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
