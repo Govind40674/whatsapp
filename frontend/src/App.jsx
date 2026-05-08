7938fb27bdf55ecd57a4c0948ec1f3feab2c3f9f
Govind40674 <govindkumar4067444@outlook.com>
2026-05-08 08:11:07 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
 import { Routes } from "react-router-dom"
 import Router from "./components/routes/Router";
+import { useEffect } from "react";
 
 
 
 
 function App() {
+  useEffect(() => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js")
+      .then((reg) => {
+        console.log("✅ SW registered", reg);
+        alert("SW registered");
+      })
+      .catch((err) => {
+        console.error("❌ SW failed", err);
+        alert("SW failed");
+      });
+  }
+}, []);
   
 
   return (
     <>
+
     <Router/>
     {/* <h1>go</h1> */}
     
