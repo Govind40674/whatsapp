4def6445295c0342a175efb7362f93b760a82230
Govind40674 <govindkumar4067444@outlook.com>
2026-05-08 08:06:11 +0530

    changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -9,11 +9,18 @@ import { getFCMToken } from "../../components/firebase/firebase";
 
 function Home() {
   useEffect(() => {
+    navigator.serviceWorker.getRegistrations().then((r) => {
+      console.log("SW Registrations:", r);
+
+      alert("SW count: " + r.length);
+    });
+  }, []);
+  useEffect(() => {
     getFCMToken();
   }, []);
+
   return (
     <>
-     
       <button
         onClick={() => {
           console.log("Button clicked");
