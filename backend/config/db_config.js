51c14055e65e2e35838d8ca77797503350306e34
Govind40674 <govindkumar4067444@outlook.com>
2026-05-09 08:36:23 +0530

    changes

--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,14 +1,19 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
+
     console.log("MongoDB connected");
+
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
+
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
